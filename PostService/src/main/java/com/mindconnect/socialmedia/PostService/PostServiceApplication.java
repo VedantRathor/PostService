80e66e6a9489b4282fe5afa43b05fe8ab9fcdd96
vedantrathore23 <vedant.rathore@heliossolutions.co>
2025-10-12 06:38:42 +0530

    cicd check

--- PostService/src/main/java/com/mindconnect/socialmedia/PostService/PostServiceApplication.java
+++ PostService/src/main/java/com/mindconnect/socialmedia/PostService/PostServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
-        // checking CICD - second time
+        // checking CICD - third time
 		SpringApplication.run(PostServiceApplication.class, args);
 		System.out.println("System running fine..");
 	}
